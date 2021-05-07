9620ba17026b353eade3169b70fabc480b8585af
Yin Song <songniy@gmail.com>
2021-05-07 16:12:28 -0400

    starting

--- sonnet_model.py
+++ sonnet_model.py
@@ -2,4 +2,18 @@ import tensorflow as tf
 import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM, Dropout, Bidirectional, Embedding,
+#can pad sequences with keras
+#spacy has tokenizer
+from keras.preprocessing.sequence import pad_sequences
+import spacy
+#decoder
 
+#encoder
+#bidrectional LSTM
+#attention tensors
+'''
+we embed context words using embedding matrices to produce a sequence of
+hidden states
+
+'''
+#output later
